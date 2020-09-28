94e2fa600d6a167de34ca4a134e2a8586b5bc66c
TIEN <tina.hsieh930301@gmail.com>
2020-09-28 15:48:53 +0800

    加入蜂鳴器

--- ArduinoSmartHome/ArduinoSmartHome.ino
+++ ArduinoSmartHome/ArduinoSmartHome.ino
@@ -3,6 +3,7 @@ void setup() {
 pinMode(12, OUTPUT);
 pinMode(11, OUTPUT);
 pinMode(13, OUTPUT);
+pinMode(7, OUTPUT);
 }
 
 void loop() {
@@ -10,6 +11,8 @@ void loop() {
 digitalWrite(12, HIGH);
 digitalWrite(11, LOW);
 
+
+tone(7, 1000, 250);
 delay(250);
 digitalWrite(12, LOW);
 digitalWrite(11, HIGH);
@@ -17,5 +20,8 @@ digitalWrite(11, HIGH);
 delay(250);
 digitalWrite(11, LOW);
 digitalWrite(13, HIGH);
-delay(250);
-digitalWrite(13, LOW);}
+tone(7, 800, 250);
+delay(500);
+digitalWrite(13, LOW);
+
+}
