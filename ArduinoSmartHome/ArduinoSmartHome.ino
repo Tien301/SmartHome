fca181357715e7fdf38c36d33c36f046a9582e87
TIEN <tina.hsieh930301@gmail.com>
2020-09-28 15:28:27 +0800

    每0.25秒交換閃的三顆燈

--- ArduinoSmartHome/ArduinoSmartHome.ino
+++ ArduinoSmartHome/ArduinoSmartHome.ino
@@ -1,13 +1,21 @@
 void setup() {
  
 pinMode(12, OUTPUT);
+pinMode(11, OUTPUT);
+pinMode(13, OUTPUT);
 }
 
 void loop() {
  
 digitalWrite(12, HIGH);
-delay(500);
-digitalWrite(12, LOW);
-delay(500);
+digitalWrite(11, LOW);
 
-}
+delay(250);
+digitalWrite(12, LOW);
+digitalWrite(11, HIGH);
+
+delay(250);
+digitalWrite(11, LOW);
+digitalWrite(13, HIGH);
+delay(250);
+digitalWrite(13, LOW);}
