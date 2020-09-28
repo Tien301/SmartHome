98b7600629c82e5b28b0edb0e10cd102ee60c213
TIEN <tina.hsieh930301@gmail.com>
2020-09-28 16:02:33 +0800

    燈and器在斷線時亮燈、發出警報

--- ArduinoSmartHome/ArduinoSmartHome.ino
+++ ArduinoSmartHome/ArduinoSmartHome.ino
@@ -2,26 +2,29 @@ void setup() {
  
 pinMode(12, OUTPUT);
 pinMode(11, OUTPUT);
-pinMode(13, OUTPUT);
+
 pinMode(7, OUTPUT);
+pinMode(2, INPUT);
+Serial.begin(9600);
 }
 
 void loop() {
- 
+  if(digitalRead(2)==HIGH) {
 digitalWrite(12, HIGH);
 digitalWrite(11, LOW);
 
-
 tone(7, 1000, 250);
-delay(250);
+delay(500);
+
 digitalWrite(12, LOW);
 digitalWrite(11, HIGH);
-
-delay(250);
-digitalWrite(11, LOW);
-digitalWrite(13, HIGH);
 tone(7, 800, 250);
 delay(500);
-digitalWrite(13, LOW);
+
+digitalWrite(11, LOW);
+
+  }
+
+
 
 }
